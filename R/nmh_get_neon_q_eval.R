dd955523bd94109c2fc8dd16dab3c58dbe482a64
weston <westonslaughter@gmail.com>
2023-01-24 11:51:12 -0500

    hydro coef rda too

---
+++ R/nmh_get_neon_q_eval.R
@@ -0,0 +1,25 @@
+#' @export
+nmh_get_neon_q_eval <- function(dest_fp = 'data/raw/qaqc/', dest_fn = 'neon_q_eval.csv', download = FALSE) {
+  if(download) {
+    destfile <- paste0(dest_fp, dest_fn)
+
+
+    if(!dir.exists(dest_fp)){
+      # create direcotry if doesnt exists
+      print(paste('Directory does not exist, creating dir at:', dest_fp))
+      dir.create(dest_fp, recursive = TRUE)
+    }
+
+    # read in the discharge evaluation from Rhea et al. (in prep)
+    download.file(
+      url = 'https://www.hydroshare.org/resource/03c52d47d66e40f4854da8397c7d9668/data/contents/neon_q_eval.csv',
+      destfile = destfile
+    )
+
+    q_eval <- read.csv(destfile)
+    return(q_eval)
+
+  } else {
+    return(nmh_q_eval)
+  }
+}

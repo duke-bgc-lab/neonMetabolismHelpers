33b13c852ad81cb89229684834527bf2f1eb6527
Nick Marzolf <37585374+nmarzolf91@users.noreply.github.com>
2024-03-05 15:00:24 -0500

    Update nmh_internals.R
    add filtering to get_neon_site_data()

--- R/nmh_internals.R
+++ R/nmh_internals.R
@@ -415,8 +415,8 @@ sample_neon_product <- function(product_codes = 'DP0.20288.001', product_name = 
 get_neon_site_data <- function(download = TRUE) {
   
   if(download){
-    dest_dir <- 'data/site_data/'
-    dest_fn <- 'neon_site_data.csv'
+    dest_dir <- 'data/'
+    dest_fn <- '1_neon_site_data.csv'
     
     destfile <- file.path(dest_dir, dest_fn)
     
@@ -430,13 +430,17 @@ get_neon_site_data <- function(download = TRUE) {
     site_data <- readr::read_csv(destfile)
     
     table_1 <- site_data %>% 
+      dplyr::filter(field_site_subtype %in% c('Wadeable Stream', 'Non-wadeable River')) %>% 
       dplyr::select(Domain = field_domain_id,
                     `Site Code` = field_site_id,
                     `Site Name` = field_site_name,
                     Latitude = field_latitude,
                     Longitude = field_longitude,
                     Elevation = field_mean_elevation_m,
-                    `Watershed Area (km2)` = field_watershed_size_km2)
+                    `Watershed Area (km2)` = field_watershed_size_km2) %>% 
+      sf::st_as_sf(.,
+                   coords = c('Longitude', 'Latitude'),
+                   crs = 4326)
     
     readr::write_csv(table_1,
                      'data/1_neon_site_data.csv')

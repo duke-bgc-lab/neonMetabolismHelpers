3d1c6cbfa0a9e19605ba84d441ada398c86bdede
weston <westonslaughter@gmail.com>
2024-03-06 17:00:41 -0500

    notes, links, cites, re necessary data source updates

--- R/nmh_get_scaling_coefs.R
+++ R/nmh_get_scaling_coefs.R
@@ -10,7 +10,9 @@ nmh_get_scaling_coefs <- function(dest_fp = 'data/raw/macrosheds/',
   }
   
   
-  # read in the discharge evaluation from Rhea et al. (in prep)
+  # NOTE: applies to this and other ismilar ufnction (1) only room in this town for one nmh_get_scaling_coef?
+  # and (2) should get this data from best available public src associated w publication, or, hosted on figshare with
+  # consent of original authors
   download.file(
     url = 'https://raw.githubusercontent.com/nmarzolf91/NEON-reaeration/master/data/derived/scaling_coefs/NEON_site_scaling_coefs.csv',
     destfile = destfile

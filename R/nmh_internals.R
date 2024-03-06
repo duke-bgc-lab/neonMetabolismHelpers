3378d6fce6015f6a073328ed3287bfe6cff6ff82
Nick Marzolf <37585374+nmarzolf91@users.noreply.github.com>
2024-03-06 14:09:53 -0500

    Update nmh_internals.R
    remove get_site_data from internals

--- R/nmh_internals.R
+++ R/nmh_internals.R
@@ -412,43 +412,6 @@ sample_neon_product <- function(product_codes = 'DP0.20288.001', product_name = 
   }
 }
 
-get_neon_site_data <- function(download = TRUE) {
-  
-  if(download){
-    dest_dir <- 'data/'
-    dest_fn <- '1_neon_site_data.csv'
-    
-    destfile <- file.path(dest_dir, dest_fn)
-    
-    if(!dir.exists(dest_dir)){
-      dir.create(dest_dir)
-    }
-    
-    download.file(url = 'https://www.neonscience.org/sites/default/files/NEON_Field_Site_Metadata_20231026.csv',
-                  destfile = destfile)
-    
-    site_data <- readr::read_csv(destfile)
-    
-    table_1 <- site_data %>% 
-      dplyr::filter(field_site_subtype %in% c('Wadeable Stream', 'Non-wadeable River')) %>% 
-      dplyr::select(Domain = field_domain_id,
-                    `Site Code` = field_site_id,
-                    `Site Name` = field_site_name,
-                    Latitude = field_latitude,
-                    Longitude = field_longitude,
-                    Elevation = field_mean_elevation_m,
-                    `Watershed Area (km2)` = field_watershed_size_km2) %>% 
-      sf::st_as_sf(.,
-                   coords = c('Longitude', 'Latitude'),
-                   crs = 4326)
-    
-    readr::write_csv(table_1,
-                     'data/1_neon_site_data.csv')
-    
-  }  
-}
-
-
 read_all_neon_feathers <- function(file_path, by_site = TRUE){
   
   if(by_site == TRUE){
